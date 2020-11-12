84e3726d11fc7d16e226265781f6a6ddfca10551
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2020-11-12 21:18:42 +0530

    Create HamiltonianAndLagrangian.java

---
+++ Nov2020/HamiltonianAndLagrangian.java
@@ -0,0 +1,24 @@
+/*
+Problem Name: Hamiltonian and Lagrangian
+Problem link: https://www.hackerearth.com/practice/data-structures/arrays/1-d/practice-problems/algorithm/hamiltonian-and-lagrangian/
+*/
+
+import java.util.*;
+class HamiltonianAndLagrangian {
+    public static void main(String args[] ) throws Exception {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = in.nextInt();
+        }
+        int max = arr[n-1];
+        String ans = arr[n-1] + "";
+        for(int i=n-2;i>=0;i--){
+            max = Math.max(max, arr[i]);
+            if(max == arr[i])
+                ans = arr[i] + " " + ans;
+        }
+        System.out.println(ans);
+    }
+}
