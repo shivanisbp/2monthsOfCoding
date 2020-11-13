b1b100a29264d089d8ad96dcdd647cdf29282b47
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2020-11-13 19:02:38 +0530

    Create MicroAndArrayUpdate.java

---
+++ Nov2020/13Nov/MicroAndArrayUpdate.java
@@ -0,0 +1,21 @@
+/*
+Problem Name: Micro and Array Update
+Problem Link: https://www.hackerearth.com/practice/data-structures/arrays/1-d/practice-problems/algorithm/micro-and-array-update/
+*/
+import java.util.*;
+class MicroAndArrayUpdate {
+    public static void main(String args[] ) throws Exception {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(;t>0;t--){
+            int n = in.nextInt(), k = in.nextInt();
+            int min = Integer.MAX_VALUE;
+            for(int i=0;i<n;i++){ 
+                int ai = in.nextInt();
+                if(ai < min)
+                    min=ai;
+            }
+            System.out.println(Math.max(k-min,0));       
+        }
+    }
+}
