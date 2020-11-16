0af28bc50b16a1e9ec339b3142212ce0737ae006
Shivani Patel <43903597+shivanisbp@users.noreply.github.com>
2020-11-16 15:36:48 +0400

    Create MarkTheAnswer.cpp

---
+++ Nov2020/16Nov/MarkTheAnswer.cpp
@@ -0,0 +1,33 @@
+/*
+Problem Name: Mark The Answer
+Problem Link: https://www.hackerearth.com/practice/data-structures/arrays/1-d/practice-problems/algorithm/mark-the-answer-1/
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);    
+    cin.tie(0);             
+    int N,X;
+    cin>>N>>X;
+    int A[N];
+    for(int i=0;i<N;++i){
+        cin>>A[i];
+    }
+
+    long long int maxscore=0,skipped=0,count=0;
+    for(int i=0;i<N;++i){
+        if(A[i]<=X){
+            count++;
+        }
+        else if(skipped<1){
+            skipped++;
+        }
+        else{
+            break;
+        }
+    }
+    maxscore=count;
+    cout<<maxscore;
+    return 0;
+}
