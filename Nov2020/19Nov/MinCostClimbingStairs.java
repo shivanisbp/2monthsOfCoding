437731fdd20e62e3794fbc694d82bbba9587b76a
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2020-11-19 13:07:26 +0530

    Create MinCostClimbingStairs.java

---
+++ Nov2020/19Nov/MinCostClimbingStairs.java
@@ -0,0 +1,13 @@
+/*
+Problem Name: 746. Min Cost Climbing Stairs
+Problem Link: https://leetcode.com/problems/min-cost-climbing-stairs/
+*/
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        for(int i=2;i<n;i++){
+            cost[i] += Math.min(cost[i-1], cost[i-2]);
+        }
+        return Math.min(cost[n-1], cost[n-2]);
+    }
+}
