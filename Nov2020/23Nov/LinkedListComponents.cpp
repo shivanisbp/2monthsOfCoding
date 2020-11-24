5b2b123a83d9542437961cef08e39c56f279c056
Shivani Patel <43903597+shivanisbp@users.noreply.github.com>
2020-11-24 21:49:27 +0400

    Create LinkedListComponents.cpp

---
+++ Nov2020/23Nov/LinkedListComponents.cpp
@@ -0,0 +1,37 @@
+/*
+Problem name: 817. Linked List Components
+Problem link: https://leetcode.com/problems/linked-list-components/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int numComponents(ListNode* head, vector<int>& G) {
+        int count=0,i=0;
+        ListNode* curr=head;
+        curr=head;
+        set<int>s(G.begin(),G.end());
+        
+        while(curr!=nullptr){
+            if(s.count(curr->val)){
+                count++;
+                while(curr!=nullptr && s.count(curr->val)){
+                    curr=curr->next;
+                }
+            }
+            else{
+                curr=curr->next;
+            }
+        }
+        return count;
+    }
+};
