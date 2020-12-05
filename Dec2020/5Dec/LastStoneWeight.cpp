4a13b5a76bbdee89b7b46554706b36edfa5d06d5
Shivani Patel <43903597+shivanisbp@users.noreply.github.com>
2020-12-05 23:43:43 +0400

    Create LastStoneWeight.cpp

---
+++ Dec2020/5Dec/LastStoneWeight.cpp
@@ -0,0 +1,26 @@
+/*
+Problem name: 1046. Last Stone Weight
+Problem link: https://leetcode.com/problems/last-stone-weight/
+*/
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue <int> pq;
+        for(int i=0;i<stones.size();++i)
+            pq.push(stones[i]);
+        
+        while(pq.size() > 1){
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+            if(a == b)
+                continue;
+            else
+                pq.push(a-b);
+        }
+        if(pq.empty())  return 0;
+        else    return pq.top();
+    }
+};
