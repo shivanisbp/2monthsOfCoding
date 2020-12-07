c7c80c8ec31631ae1c3820d6ca2b1a592f31d05a
Shivani Patel <43903597+shivanisbp@users.noreply.github.com>
2020-12-08 01:28:28 +0400

    Update SumOfAllOddLengthSubarrays.cpp

--- Dec2020/7Dec/SumOfAllOddLengthSubarrays.cpp
+++ Dec2020/7Dec/SumOfAllOddLengthSubarrays.cpp
@@ -7,17 +7,16 @@ class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
         int sum = 0;
-        for(int i=0;i<arr.size();i++){
-            int s=0,x=0;
-            for(int j=i;j<arr.size();j++){
-                s+=arr[j];
-                if(x%2==0){
-                    sum+=s;
+        for(int i=0;i<arr.size();++i){
+            int a=0,b=0;
+            for(int j=i;j<arr.size();++j){
+                a += arr[j];
+                if(b%2==0){
+                    sum+=a;
                 }
-                x++;
+                b++;
             }
         }
        return sum;
-        
     }
 };
