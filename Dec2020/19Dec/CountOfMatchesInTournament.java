b775b5156bf964eacb320d8fff7e5990b20f4b09
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2020-12-19 19:02:49 +0530

    Create CountOfMatchesInTournament.java

---
+++ Dec2020/19Dec/CountOfMatchesInTournament.java
@@ -0,0 +1,9 @@
+/*
+Problem Name: 1688. Count of Matches in Tournament
+Problem Link: https://leetcode.com/problems/count-of-matches-in-tournament/
+*/
+class Solution {
+    public int numberOfMatches(int n) {
+        return n - 1;
+    }
+}
