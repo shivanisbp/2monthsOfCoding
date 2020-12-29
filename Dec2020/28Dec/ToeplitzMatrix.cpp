7aaaed5282baef7dc0fe54378a8755a2d3131127
Shivani Patel <43903597+shivanisbp@users.noreply.github.com>
2020-12-29 14:09:23 +0400

    Create ToeplitzMatrix.cpp

---
+++ Dec2020/28Dec/ToeplitzMatrix.cpp
@@ -0,0 +1,17 @@
+/*
+Problem name: 766. Toeplitz Matrix
+Problem link: https://leetcode.com/problems/toeplitz-matrix/
+*/
+
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        for(int i=1;i<matrix.size();++i){
+            for(int j=1;j<matrix[i].size();++j){
+                if(matrix[i][j] != matrix[i-1][j-1])
+                    return false;
+            }
+        }
+        return true;
+    }
+};
