f45d17380ebe07c5ad536489575de768608eb4b9
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2020-12-30 22:22:09 +0530

    Update ImageSmoother.java

--- Dec2020/30Dec/ImageSmoother.java
+++ Dec2020/30Dec/ImageSmoother.java
@@ -1,24 +1,23 @@
 /*
 Problem Name: 661. Image Smoother
-Problem Link: https://leetcode.com/problems/relative-ranks/
+Problem Link: https://leetcode.com/problems/image-smoother/
 */
 class Solution {
-    public String[] findRelativeRanks(int[] nums) {
-        TreeMap<Integer,Integer> map = new TreeMap<Integer,Integer>(Collections.reverseOrder());
-        int n = nums.length;
-        for(int i=0;i<n;i++){
-            map.put(nums[i],i);
-        }
-        String ans[] = new String[nums.length];
-        Iterator<Integer> itr = map.keySet().iterator(); 
-        if(n >= 1)
-            ans[map.get(itr.next())] = "Gold Medal";
-        if(n >= 2)
-            ans[map.get(itr.next())] = "Silver Medal";
-        if(n >= 3)
-            ans[map.get(itr.next())] = "Bronze Medal";
-        for(int i=3;i<n;i++){
-            ans[map.get(itr.next())] = (i+1) + "";
+    public int[][] imageSmoother(int[][] M) {
+        int m = M.length, n = M[0].length;
+        int ans[][] = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int sum = 0, cnt = 0;
+                for(int ii = i-1;ii<=i+1;ii++){
+                    for(int jj = j-1;jj<=j+1;jj++)
+                        if(ii >= 0 && ii < m && jj >= 0 && jj < n){
+                            sum += M[ii][jj];
+                            cnt++;
+                        }
+                }
+                ans[i][j] = sum/cnt;
+            }
         }
         return ans;
     }
