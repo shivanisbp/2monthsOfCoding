49cc11ce9e9da3b2c66352fe0269271871bd1db8
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2020-12-30 22:22:43 +0530

    Create RelativeRanks.java

---
+++ Dec2020/30Dec/RelativeRanks.java
@@ -0,0 +1,25 @@
+/*
+Problem Name: 506. Relative Ranks
+Problem Link: https://leetcode.com/problems/relative-ranks/
+*/
+class Solution {
+    public String[] findRelativeRanks(int[] nums) {
+        TreeMap<Integer,Integer> map = new TreeMap<Integer,Integer>(Collections.reverseOrder());
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            map.put(nums[i],i);
+        }
+        String ans[] = new String[nums.length];
+        Iterator<Integer> itr = map.keySet().iterator(); 
+        if(n >= 1)
+            ans[map.get(itr.next())] = "Gold Medal";
+        if(n >= 2)
+            ans[map.get(itr.next())] = "Silver Medal";
+        if(n >= 3)
+            ans[map.get(itr.next())] = "Bronze Medal";
+        for(int i=3;i<n;i++){
+            ans[map.get(itr.next())] = (i+1) + "";
+        }
+        return ans;
+    }
+}
