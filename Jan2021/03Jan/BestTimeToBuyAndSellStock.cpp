f8bb06c62d95985342db648fd9c0c8704ad0cb45
Shivani Patel <43903597+shivanisbp@users.noreply.github.com>
2021-01-03 23:47:14 +0400

    Update BestTimeToBuyAndSellStock.cpp

--- Jan2021/03Jan/BestTimeToBuyAndSellStock.cpp
+++ Jan2021/03Jan/BestTimeToBuyAndSellStock.cpp
@@ -1,12 +1,15 @@
 /*
 Problem name: Best Time to Buy and Sell Stock
-Problem link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+Problem link:
 */
+
 
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
+        if(n<2)
+            return 0;
         int maxprofit=0, rightmax=prices[n-1];
         
         for(int i=n-2 ;i>=0 ;--i){
